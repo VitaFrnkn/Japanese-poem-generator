4f0cea58a370df2a346b42ddf70431c0444b799a
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2025-03-05 13:40:39 +0900

    Api call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generatePoem(event){
-    event.preventDefault();
-
-    
+function displayPoem(response){
+       
     new Typewriter('#poem', {
-      strings: 'ひらひらと かぜにまいちる はるのゆめ 🌸',
+      strings:response.data.answer,
       autoStart: true,
       delay: 5,
       cursor: "",
     });
+}
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let inputInstructions = document.querySelector("#input-instructions");
+    let apiKey = "oe1f0572f4bt753a71ffbc4045676795";
+    let prompt = `Generate a poem in Japanese about ${inputInstructions}. There is no title. Separate each line using <br />. Please follow the instructions for every entry. Sign it only once at the end, as CyberV and place the signature in between <strong> </strong>`;
+    let context = "You are specialised in short romantic Japanese poetry"
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#form");
