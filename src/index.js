ec5b4c81f07ca870e58ff896e172161e90e0f06a
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2025-03-11 14:20:39 +0900

    Added better User experience such as blinking while generating

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event){
 
     let inputInstructions = document.querySelector("#input-instructions");
     let apiKey = "oe1f0572f4bt753a71ffbc4045676795";
-    let prompt = `Generate a poem in Japanese about ${inputInstructions}. There is no title. Separate each line using <br />. Please follow the instructions for every entry and be as accurate as possible. Write the translation in english after each line.`;
+    let prompt = `Generate a poem in Japanese about ${inputInstructions}. There is no title. Separate each line using <br />. Please follow the instructions for every entry and be as accurate as possible. Write the translation in english. Separate each line using <br />. Always generate in the same format`;
     let context = "You are specialised in short romantic Japanese poetry"
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+let poemElement=document.querySelector("#poem");
+
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating"> Generating a Japanese poem </div>`;
 
     axios.get(apiUrl).then(displayPoem);
 }
